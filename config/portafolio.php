65f2e45fb01de9c32d55848316bb503d394602c7
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-12-01 16:00:09 -0600

    add: Add config file for portafolio

---
+++ config/portafolio.php
@@ -0,0 +1,36 @@
+<?php
+
+
+return [
+
+
+    'Colegio Los Ángeles' => [
+
+        'img' => 'resources/img/cla-imagen.png',
+
+        'imgAlt' => 'CLA',
+
+        'descripcion' => '
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi 
+            architecto non optio quod porro sapiente, atque, reprehenderit, quidem quis soluta at! 
+            Velit voluptatum qui eveniet inventore necessitatibus placeat aliquid?
+        ',
+
+        'imgLogo' => 'resources/img/cla-logo.png',
+
+        'listaServicios' => [
+            'Social Media',
+            'Fotografía',
+            'Video',
+            'Publicidad Impresa',
+            'Publicidad Exterior',
+            'Diseño Editorial'
+        ],
+
+        'imgSize' => 'portafolio__proyecto-logo'
+
+        
+    ]
+
+
+];
